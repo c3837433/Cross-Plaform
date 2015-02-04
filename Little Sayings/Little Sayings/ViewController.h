14eb52110625412c648229a10cf62aa4edeff265
Angessmith <angessmith@fullsail.edu>
2015-02-04 14:13:45 -0600

    Started iOS version of application.

---
+++ Little Sayings/Little Sayings/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Little Sayings
+//
+//  Created by Angela Smith on 2/4/15.
+//  Copyright (c) 2015 Angela Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
